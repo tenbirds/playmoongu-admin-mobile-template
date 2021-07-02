95d0c4b564b2cf2e7e5800580a4f1669bf19f6b9
Kevin <mac@MacBook-Pro-cua-Mac.local>
2021-07-02 09:40:31 +0700

    update nav

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,9 @@ function handleClick(value){
         $(`#${value}`).modal('show');
     }
 }
+function openNav(){
+    $(`#main-navigation`).addClass('open')
+}
 function closeNav(){
     $('#main-navigation').removeClass('open')
 }
